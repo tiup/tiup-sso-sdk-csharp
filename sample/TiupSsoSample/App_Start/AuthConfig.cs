89700c162d7a13195dd2b9b13837133308f7145c
linx <linx@monokeros.cn>
2015-01-09 20:37:37 +0800

    clean and first preview

---
+++ sample/TiupSsoSample/App_Start/AuthConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using TiupSsoSample.Models;
+using DotNetOpenAuth;
+using DotNetOpenAuth.TiupSso;
+
+namespace TiupSsoSample
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            var client = new TiupSso("yourClientId", "yourClientSecret");
+            var extraData = new Dictionary<string, object>();
+            OAuthWebSecurity.RegisterClient(client, "Tiup.cn", extraData);
+        }
+    }
+}
