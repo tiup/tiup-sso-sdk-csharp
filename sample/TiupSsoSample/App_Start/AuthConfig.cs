d4835e0da477891747ba0a143122ceeb8b2f34e3
linx <linx@monokeros.cn>
2015-01-13 11:01:29 +0800

    remove test appId and app secret

--- sample/TiupSsoSample/App_Start/AuthConfig.cs
+++ sample/TiupSsoSample/App_Start/AuthConfig.cs
@@ -19,7 +19,7 @@ namespace TiupSsoSample
             extraData["theme"] = "schools";
             extraData["sso"] = "true";
             //input your appId and  app secret
-            var client = new TiupSso("cn.tiup.ruc", "15d00623a33b1396d66042ec1dd581b71ec3ed68ce6099afcb0217857a375d17", extraData, new[] { "all" });
+            var client = new TiupSso("Your AppId", "You App Secret", extraData, new[] { "all" });
             OAuthWebSecurity.RegisterClient(client, "Tiup.cn", null);
         }
     }
